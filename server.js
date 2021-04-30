af4ea3ff6bfefea15cb018e67d45366435047d18
Mark Raudenbush <markraud@hotmail.com>
2021-04-30 00:23:17 -0600

    posts new note to the db.json file

--- server.js
+++ server.js
@@ -32,9 +32,13 @@ app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.j
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.post('/api/notes', (req, res) => {
+  // const file = req.body;
+  // console.log(file);
+
   db.push(req.body);
-  console.log(db);
-  // fs.writeFile(db, file, (err) =>  err ? console.error(err) : console.log('Success!'))
+  // console.log(db);
+  // const data = JSON.stringify(db);
+  fs.writeFileSync('./db/db.json', JSON.stringify(db), (err) =>  err ? console.error(err) : console.log('Success!'))
 });
 
 // Listener
