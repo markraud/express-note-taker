5880a1233b8f7a5e127035b1bccbe3ab04a0f116
Mark Raudenbush <markraud@hotmail.com>
2021-04-29 23:47:58 -0600

    worked on post for api/notes and have it pushing new note to object but not writing to file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+let db = require('./db/db.json');
 
 const app = express();
 const PORT = 3000;
@@ -20,7 +22,6 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
@@ -29,6 +30,12 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/note
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.post('/api/notes', (req, res) => {
+  db.push(req.body);
+  console.log(db);
+  // fs.writeFile(db, file, (err) =>  err ? console.error(err) : console.log('Success!'))
+});
 
 // Listener
 app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
